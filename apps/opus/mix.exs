f80d6bebfb12e2b9f49466248c1c5e99aa0e35ee
moonmoon69 <terryli.bizwiz@gmail.com>
2026-02-14 20:59:03 +0700

    v0.11.8

--- apps/opus/mix.exs
+++ apps/opus/mix.exs
@@ -4,7 +4,7 @@ defmodule Opus.MixProject do
   def project do
     [
       app: :opus,
-      version: "0.11.7",
+      version: "0.11.8",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
