4d47ca3bb0f170944f7c43679aebf3998181ceb1
moonmoon69 <terryli.bizwiz@gmail.com>
2026-02-14 17:20:44 +0700

    v0.11.7

--- apps/compendium/mix.exs
+++ apps/compendium/mix.exs
@@ -4,7 +4,7 @@ defmodule Compendium.MixProject do
   def project do
     [
       app: :compendium,
-      version: "0.11.6",
+      version: "0.11.7",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
