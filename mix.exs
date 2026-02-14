35aac2cac42cc194393de25a72a74878678b967b
moonmoon69 <terryli.bizwiz@gmail.com>
2026-02-14 08:54:56 +0700

    v0.11.6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Cyfr.MixProject do
     [
       apps_path: "apps",
       apps: [:arca, :compendium, :emissary, :locus, :opus, :sanctum, :sanctum_arx],
-      version: "0.11.5",
+      version: "0.11.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
