d8e71bd7114833403036040d37ba64d5686e11a4
moonmoon69 <terryli.bizwiz@gmail.com>
2026-02-15 19:02:14 +0700

    v0.11.9

--- apps/sanctum/mix.exs
+++ apps/sanctum/mix.exs
@@ -7,7 +7,7 @@ defmodule Sanctum.MixProject do
   def project do
     [
       app: :sanctum,
-      version: "0.11.8",
+      version: "0.11.9",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Cyfr.MixProject do
     [
       apps_path: "apps",
       apps: [:arca, :compendium, :emissary, :locus, :opus, :sanctum, :sanctum_arx],
-      version: "0.11.8",
+      version: "0.11.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
