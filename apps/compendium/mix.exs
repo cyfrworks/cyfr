0cf241e15adab08a3998444784eda1341068c91f
moonmoon69 <terryli.bizwiz@gmail.com>
2026-02-16 19:51:29 +0700

    v0.11.12

--- apps/compendium/mix.exs
+++ apps/compendium/mix.exs
@@ -4,7 +4,7 @@ defmodule Compendium.MixProject do
   def project do
     [
       app: :compendium,
-      version: "0.11.11",
+      version: "0.11.12",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- apps/sanctum/mix.exs
+++ apps/sanctum/mix.exs
@@ -7,7 +7,7 @@ defmodule Sanctum.MixProject do
   def project do
     [
       app: :sanctum,
-      version: "0.11.11",
+      version: "0.11.12",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
