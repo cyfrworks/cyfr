78b42a315a98393ab31b524fff4e219e439761cd
moonmoon69 <terryli.bizwiz@gmail.com>
2026-02-17 20:18:55 +0700

    v0.12.4

--- apps/prism/mix.exs
+++ apps/prism/mix.exs
@@ -4,7 +4,7 @@ defmodule Prism.MixProject do
   def project do
     [
       app: :prism,
-      version: "0.12.3",
+      version: "0.12.4",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
