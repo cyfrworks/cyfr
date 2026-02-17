b776fefea982f5612a47260b60d342f18cfc15a2
moonmoon69 <terryli.bizwiz@gmail.com>
2026-02-17 16:26:58 +0700

    v0.12.0

--- apps/prism/mix.exs
+++ apps/prism/mix.exs
@@ -4,7 +4,7 @@ defmodule Prism.MixProject do
   def project do
     [
       app: :prism,
-      version: "0.11.13",
+      version: "0.12.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Cyfr.MixProject do
     [
       apps_path: "apps",
       apps: [:arca, :compendium, :emissary, :locus, :opus, :prism, :sanctum, :sanctum_arx],
-      version: "0.11.13",
+      version: "0.12.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
