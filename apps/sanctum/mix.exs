7442d0e5cbaef48b336e960b3f0eede44861f369
moonmoon69 <terryli.bizwiz@gmail.com>
2026-02-18 20:19:50 +0700

    v0.12.7

--- apps/sanctum/mix.exs
+++ apps/sanctum/mix.exs
@@ -7,7 +7,7 @@ defmodule Sanctum.MixProject do
   def project do
     [
       app: :sanctum,
-      version: "0.12.6",
+      version: "0.12.7",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
