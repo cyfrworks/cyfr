c5fc92d4e69f2d6e7323b78198dc80ccecdd8c84
moonmoon69 <terryli.bizwiz@gmail.com>
2026-02-20 21:07:45 +0700

    v0.12.10

--- apps/prism/mix.exs
+++ apps/prism/mix.exs
@@ -4,7 +4,7 @@ defmodule Prism.MixProject do
   def project do
     [
       app: :prism,
-      version: "0.12.9",
+      version: "0.12.10",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- apps/sanctum/mix.exs
+++ apps/sanctum/mix.exs
@@ -7,7 +7,7 @@ defmodule Sanctum.MixProject do
   def project do
     [
       app: :sanctum,
-      version: "0.12.9",
+      version: "0.12.10",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
