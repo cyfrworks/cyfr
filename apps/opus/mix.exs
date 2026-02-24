861becae94d67d10194a48ba8ceb0e849636f330
moonmoon69 <terryli.bizwiz@gmail.com>
2026-02-24 11:12:47 +0700

    v0.13.4

--- apps/opus/mix.exs
+++ apps/opus/mix.exs
@@ -4,7 +4,7 @@ defmodule Opus.MixProject do
   def project do
     [
       app: :opus,
-      version: "0.13.3",
+      version: "0.13.4",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- apps/sanctum/mix.exs
+++ apps/sanctum/mix.exs
@@ -7,7 +7,7 @@ defmodule Sanctum.MixProject do
   def project do
     [
       app: :sanctum,
-      version: "0.13.3",
+      version: "0.13.4",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
