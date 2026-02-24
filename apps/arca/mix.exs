b727aae39d64ad157de86e38e3565643f082730b
moonmoon69 <terryli.bizwiz@gmail.com>
2026-02-24 09:07:48 +0700

    v0.13.3

--- apps/arca/mix.exs
+++ apps/arca/mix.exs
@@ -4,7 +4,7 @@ defmodule Arca.MixProject do
   def project do
     [
       app: :arca,
-      version: "0.13.2",
+      version: "0.13.3",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Cyfr.MixProject do
     [
       apps_path: "apps",
       apps: [:arca, :compendium, :emissary, :locus, :opus, :prism, :sanctum, :sanctum_arx],
-      version: "0.13.2",
+      version: "0.13.3",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
