4f667d4453ec2ac796a2f4e3ff6905629197be8f
moonmoon69 <terryli.bizwiz@gmail.com>
2026-02-25 19:21:52 +0700

    v0.13.5

--- apps/emissary/mix.exs
+++ apps/emissary/mix.exs
@@ -4,7 +4,7 @@ defmodule Emissary.MixProject do
   def project do
     [
       app: :emissary,
-      version: "0.13.4",
+      version: "0.13.5",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- apps/sanctum/mix.exs
+++ apps/sanctum/mix.exs
@@ -7,7 +7,7 @@ defmodule Sanctum.MixProject do
   def project do
     [
       app: :sanctum,
-      version: "0.13.4",
+      version: "0.13.5",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
