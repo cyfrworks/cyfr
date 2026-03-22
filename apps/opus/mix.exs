496368b8f0bb04b2dec4eeda655ad61b316b76c0
moonmoon69 <terryli.bizwiz@gmail.com>
2026-03-22 21:30:51 +0700

    Fixed the external MCP Server dispatcher for Porta

--- apps/opus/mix.exs
+++ apps/opus/mix.exs
@@ -4,7 +4,7 @@ defmodule Opus.MixProject do
   def project do
     [
       app: :opus,
-      version: "1.2.4",
+      version: "1.2.5",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
