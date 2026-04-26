addd09e003ecb79b7f2c8276007eee0d55e0a916
moonmoon69 <terryli.bizwiz@gmail.com>
2026-04-26 18:39:46 +0700

    v1.6.1

--- apps/cyfr/mix.exs
+++ apps/cyfr/mix.exs
@@ -4,7 +4,7 @@ defmodule Cyfr.App.MixProject do
   def project do
     [
       app: :cyfr,
-      version: "1.6.0",
+      version: "1.6.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
